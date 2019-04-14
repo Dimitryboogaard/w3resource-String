a04d5c6f989dc678f8488f213e49238aa3845a60
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2019-04-14 20:31:35 +0200

    Add files via upload

---
+++ 2/2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string strInvoer;
+        int intTeller;
+        
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            strInvoer = tbInvoer.Text;
+
+            foreach(char chrKarakter in strInvoer)
+            {
+                intTeller++;
+            }
+
+            tbUitvoer.Text = intTeller.ToString();
+        }
+    }
+}
---
+++ 3/3/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int intTeller, intStringLengte;
+        string strInvoer;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            strInvoer = tbInvoer.Text;
+            intStringLengte = strInvoer.Length;
+
+            for(intTeller = 0; intTeller < intStringLengte; intTeller++)
+            {
+                tbUitvoer.Text += strInvoer.Substring(intTeller, 1) + " ";
+            }
+        }
+    }
+}
