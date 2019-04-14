b38533a585b3c226b4c2f8a2565b9e2766b379b6
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2019-04-14 20:48:58 +0200

    Add files via upload

---
+++ 36/36/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _36
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string strString1 = "Don't count your chickens, ";
+        string strString2 = "before the eggs, ";
+        string strString3 = "have hatched.";
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            lblAntwoord.Text = strString1 + strString2 + strString3;
+        }
+    }
+}
---
+++ 38/38/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _38
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int intIndex;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            string strZin = "Kill two birds with one stone";
+            string strSubString = "birds";
+            bool booJaOfNee = strZin.Contains(strSubString);
+
+            if(booJaOfNee)
+            {
+                intIndex = strZin.IndexOf(strSubString);
+
+                if(intIndex >= 0)
+                {
+                    lblAntwoord.Text = strSubString + " begint op positie " + intIndex++;
+                }
+            }
+        }
+    }
+}
