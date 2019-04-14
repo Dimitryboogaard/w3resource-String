dbbf4ffb4899f6fcac358d65fe9612670b1dbecf
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2019-04-14 20:49:49 +0200

    Add files via upload

---
+++ 41/41/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace _41
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAntwoord = new System.Windows.Forms.Button();
+            this.rtUitvoer = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnAntwoord
+            // 
+            this.btnAntwoord.Location = new System.Drawing.Point(12, 12);
+            this.btnAntwoord.Name = "btnAntwoord";
+            this.btnAntwoord.Size = new System.Drawing.Size(75, 23);
+            this.btnAntwoord.TabIndex = 0;
+            this.btnAntwoord.Text = "Antwoord";
+            this.btnAntwoord.UseVisualStyleBackColor = true;
+            this.btnAntwoord.Click += new System.EventHandler(this.btnAntwoord_Click);
+            // 
+            // rtUitvoer
+            // 
+            this.rtUitvoer.Location = new System.Drawing.Point(12, 41);
+            this.rtUitvoer.Name = "rtUitvoer";
+            this.rtUitvoer.Size = new System.Drawing.Size(371, 138);
+            this.rtUitvoer.TabIndex = 1;
+            this.rtUitvoer.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 189);
+            this.Controls.Add(this.rtUitvoer);
+            this.Controls.Add(this.btnAntwoord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAntwoord;
+        private System.Windows.Forms.RichTextBox rtUitvoer;
+    }
+}
+
---
+++ 41/41/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _41
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String[] strStrings = { "Actions speak louder than words", "Hello!", "Python.",
+                           "PHP.", "random" };
+        bool booEindigtMetEenPunt;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            foreach(var String in strStrings)
+            {
+                booEindigtMetEenPunt = String.EndsWith(".");
+                rtUitvoer.Text += String + " eindigt met een punt? " + booEindigtMetEenPunt + Environment.NewLine;
+            }
+        }
+    }
+}
